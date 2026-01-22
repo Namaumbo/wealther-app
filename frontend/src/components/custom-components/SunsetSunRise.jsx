50ca23579163e78e6cba3108ef0ba740895d7579
Namaumbo <daelo.namaumbo@gmail.com>
2026-01-22 11:43:33 +0200

    Update SunsetSunRise component height for improved layout and adjust MainLayout for better spacing

--- frontend/src/components/custom-components/SunsetSunRise.jsx
+++ frontend/src/components/custom-components/SunsetSunRise.jsx
@@ -4,7 +4,7 @@ import LocationSuns from "./locationSuns";
 
 const SunsetSunRise = () => {
     return (
-        <div className="p-3 w-[38em] bg-white h-[32em] rounded-3xl shadow-[0_6px_px_0_rgba(90,145,255,0.10),0_1.5px_6px_rgba(0,0,0,0.045)] border border-[#ffffff] transition-shadow duration-300 hover:shadow-[0_10px_36px_0_rgba(90,145,255,0.16),0_4px_10px_rgba(0,0,0,0.08)]">
+        <div className="p-3 w-[38em] bg-white h-[30.4em] rounded-3xl shadow-[0_6px_px_0_rgba(90,145,255,0.10),0_1.5px_6px_rgba(0,0,0,0.045)] border border-[#ffffff] transition-shadow duration-300 hover:shadow-[0_10px_36px_0_rgba(90,145,255,0.16),0_4px_10px_rgba(0,0,0,0.08)]">
             <div className="font-bold flex items-center justify-between m-7">
                 <p>Sunset and Sunrise</p>
                 <CirclePlus color="gray" />
