39459e4a14bfed7262d3a833c5a55884736daf27
Namaumbo <daelo.namaumbo@gmail.com>
2026-02-25 20:24:54 +0200

    rf

--- frontend/src/components/custom-components/AirQuality.jsx
+++ frontend/src/components/custom-components/AirQuality.jsx
@@ -2,7 +2,6 @@ import airs from "../../../data/airs.json";
 
 const AirQuality = () => {
 
-
     return (
         <div className="p-3 w-[39em] bg-white h-[15em] rounded-3xl shadow-[0_6px_px_0_rgba(90,145,255,0.10),0_1.5px_6px_rgba(0,0,0,0.045)] border border-[#ffffff] transition-shadow duration-300 hover:shadow-[0_10px_36px_0_rgba(90,145,255,0.16),0_4px_10px_rgba(0,0,0,0.08)]">
             <h1 className="text-sm font-bold">Air Quality Index</h1>
